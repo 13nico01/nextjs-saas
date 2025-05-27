6f7b674d4bd4e15e7020950834b42c668b61e076
13nico01 <nico.z130601@gmail.com>
2025-05-27 11:28:02 +0200

    implement clerk and supabase integration for creating new companions

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -23,6 +23,8 @@ import {
 } from "@/components/ui/select"
 import { subjects } from "@/constants"
 import { Textarea } from "./ui/textarea"
+import { createCompanion } from "@/lib/actions/companion.actions"
+import { redirect } from "next/navigation"
 
 
 const formSchema = z.object({
@@ -48,10 +50,15 @@ const CompanionForm = () => {
         },
     })
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const companion = await createCompanion(values);
+
+        if (companion) {
+            redirect(`/companions/${companion.id}`);
+        } else {
+            console.log('Failed to create a companion')
+            redirect('/')
+        }
     }
     return (
         <Form {...form}>
